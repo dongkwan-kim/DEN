36b2a3a5090153aec528daa28a050f0b9d840f35
Dongkwan Kim <todoaskit@gmail.com>
2019-04-25 17:23:24 +0900

    Remove dependencies (for tf-gpu)

--- setup.py
+++ setup.py
@@ -28,8 +28,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "numpy",
-        "tensorflow",
         'termcolor',
     ],
     zip_safe=False,
